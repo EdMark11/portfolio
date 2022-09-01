a9dfc4bb6016288fc469b8bed1a8de1a81e5f10f
edgarasmarkunas <markunas11@gmail.com>
2022-09-01 13:16:59 +0100

    added home and about components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -32,7 +33,24 @@ const Navbar = () => {
         </ul>
 
         {/* social icons */}
-        <div className='hidden'>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+                <a className='flex justify-between items-center w-full text-gray-300' href="/">
+                    LinkedIn <FaLinkedin size={30}/>
+                </a>
+            </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                <a className='flex justify-between items-center w-full text-gray-300' href="/">
+                    GitHub <FaGithub size={30}/>
+                </a>
+            </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+                <a className='flex justify-between items-center w-full text-gray-300' href="/">
+                    LinkedIn <HiOutlineMail size={30}/>
+                </a>
+            </li>
+        </ul>
 
         </div>
     </div>

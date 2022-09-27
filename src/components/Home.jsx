63bba34c9ae4a5d6076e500a3155ce2f615ee4ef
edgarasmarkunas <markunas11@gmail.com>
2022-09-27 15:12:10 +0100

    deployment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <h1 className='text text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Edgaras Markunas</h1>
             <h2 className='text text-4xl sm:text-6xl font-bold text-[#8892b0]'>Full Stack developer</h2>
-            <p className='text-[8892b0] py-4 max-w-[700px]'>Currently designing and building full-stack applications with responsiveness in mind!</p> 
+            <p className='text-[8892b0] py-4 max-w-[700px] text-[#8892b0]'>Currently designing and building full-stack applications with responsiveness in mind!</p> 
             <div> 
                 <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work 
                 <span className='group-hover:rotate-90 duration-300'>

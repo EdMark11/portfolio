d03382027c894d98991ef9f286e49c3a89b1d9f2
edgarasmarkunas <markunas11@gmail.com>
2023-05-16 20:01:25 +0100

    added images for skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
-/* import HTML logo from assets */
+import React from 'react';
+import html from '../assets/html.png';
+import css from '../assets/css.png';
+import javascript from '../assets/javascript.png';
+import node from '../assets/node.png';
+import reactImg from '../assets/reactImg.png';
+import postgresql from '../assets/postgresql.png';
+
+
 
 const Skills = () => {
   return (
@@ -8,33 +15,33 @@ const Skills = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600  '>Skills</p>
-                <p className='py-4'>These are the technologies I have worked with</p>
+                <p className='py-4'>These are the some of the technologies I have worked with:</p>
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    {/* <img className='w-20 mx-auto' src={HTML} alt="HTML icon" /> */}
+                    <img className='w-20 mx-auto' src={html} alt="HTML icon" /> 
                     <p className='my-4'>HTML</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    {/* <img className='w-20 mx-auto' src={CSS} alt="CSS icon" /> */}
+                    <img className='w-20 mx-auto' src={css} alt="CSS icon" /> 
                     <p className='my-4'>CSS</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    {/* <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript icon" /> */}
+                    <img className='w-20 mx-auto' src={javascript} alt="JavaScript icon" /> 
                     <p className='my-4'>JavaScript</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    {/* <img className='w-20 mx-auto' src={ReactImg} alt="React icon" /> */}
+                    <img className='w-20 mx-auto' src={reactImg} alt="React icon" /> 
                     <p className='my-4'>React</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    {/* <img className='w-20 mx-auto' src={Node} alt="Node icon" /> */}
+                    <img className='w-20 mx-auto' src={node} alt="Node icon" />
                     <p className='my-4'>Node JS</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    {/* <img className='w-20 mx-auto' src={postgreSQL} alt="postgreSQL icon" /> */}
-                    <p className='my-4'>postgreSQL</p>
+                    <img className='w-20 mx-auto' src={postgresql} alt="postgreSQL icon" /> 
+                    <p className='my-4'>PostgreSQL</p>
                 </div>
             </div>
         </div>
